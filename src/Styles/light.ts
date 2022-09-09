151f605a330932fb5687af0de2e2c55fb116c55d
Lucas Lara <lucasml@live.com>
2022-09-09 18:11:27 -0300

    Dark and light themes

---
+++ src/Styles/light.ts
@@ -0,0 +1,13 @@
+const lightTheme = {
+  name: 'light',
+  colors:{
+    primary: '#07090F',
+    secondary:'#1f2022',
+    lightGray:"#dbdbdb",
+
+    backgroundColor: "#fafafafa",
+    textColor: '#fff'
+  }
+}
+
+export default lightTheme
